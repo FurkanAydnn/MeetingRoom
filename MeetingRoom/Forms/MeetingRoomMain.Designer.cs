8d37ce01ceb5994a5000171258ea8a5178de9ce9
Furkan <furkanaydn67@gmail.com>
2019-01-10 08:21:14 +0300

    Minor Commit

--- MeetingRoom/Forms/MeetingRoomMain.Designer.cs
+++ MeetingRoom/Forms/MeetingRoomMain.Designer.cs
@@ -32,34 +32,30 @@
             this.btnCreateRoom = new System.Windows.Forms.Button();
             this.btnCreateMeeting = new System.Windows.Forms.Button();
             this.btnMeetingCancel = new System.Windows.Forms.Button();
-            this.btnDeleteRoom = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // flpRooms
             // 
-            this.flpRooms.Location = new System.Drawing.Point(16, 84);
-            this.flpRooms.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.flpRooms.Location = new System.Drawing.Point(12, 68);
             this.flpRooms.Name = "flpRooms";
-            this.flpRooms.Size = new System.Drawing.Size(635, 480);
+            this.flpRooms.Size = new System.Drawing.Size(476, 390);
             this.flpRooms.TabIndex = 0;
             // 
             // btnCreateRoom
             // 
-            this.btnCreateRoom.Location = new System.Drawing.Point(17, 16);
-            this.btnCreateRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreateRoom.Location = new System.Drawing.Point(535, 419);
             this.btnCreateRoom.Name = "btnCreateRoom";
-            this.btnCreateRoom.Size = new System.Drawing.Size(109, 48);
+            this.btnCreateRoom.Size = new System.Drawing.Size(82, 39);
             this.btnCreateRoom.TabIndex = 1;
-            this.btnCreateRoom.Text = "Yeni Oda";
+            this.btnCreateRoom.Text = "Oda Ekle/Sil";
             this.btnCreateRoom.UseVisualStyleBackColor = true;
             this.btnCreateRoom.Click += new System.EventHandler(this.btnCreateRoom_Click);
             // 
             // btnCreateMeeting
             // 
-            this.btnCreateMeeting.Location = new System.Drawing.Point(713, 15);
-            this.btnCreateMeeting.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreateMeeting.Location = new System.Drawing.Point(535, 12);
             this.btnCreateMeeting.Name = "btnCreateMeeting";
-            this.btnCreateMeeting.Size = new System.Drawing.Size(109, 48);
+            this.btnCreateMeeting.Size = new System.Drawing.Size(82, 39);
             this.btnCreateMeeting.TabIndex = 2;
             this.btnCreateMeeting.Text = "Rezervasyon";
             this.btnCreateMeeting.UseVisualStyleBackColor = true;
@@ -67,35 +63,22 @@
             // 
             // btnMeetingCancel
             // 
-            this.btnMeetingCancel.Location = new System.Drawing.Point(713, 70);
-            this.btnMeetingCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMeetingCancel.Location = new System.Drawing.Point(535, 57);
             this.btnMeetingCancel.Name = "btnMeetingCancel";
-            this.btnMeetingCancel.Size = new System.Drawing.Size(109, 48);
+            this.btnMeetingCancel.Size = new System.Drawing.Size(82, 39);
             this.btnMeetingCancel.TabIndex = 3;
             this.btnMeetingCancel.Text = "Rezervasyon İptal";
             this.btnMeetingCancel.UseVisualStyleBackColor = true;
             // 
-            // btnDeleteRoom
-            // 
-            this.btnDeleteRoom.Location = new System.Drawing.Point(713, 516);
-            this.btnDeleteRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnDeleteRoom.Name = "btnDeleteRoom";
-            this.btnDeleteRoom.Size = new System.Drawing.Size(109, 48);
-            this.btnDeleteRoom.TabIndex = 4;
-            this.btnDeleteRoom.Text = "Oda Sil";
-            this.btnDeleteRoom.UseVisualStyleBackColor = true;
-            // 
             // MeetingRoomMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(839, 578);
-            this.Controls.Add(this.btnDeleteRoom);
+            this.ClientSize = new System.Drawing.Size(629, 470);
             this.Controls.Add(this.btnMeetingCancel);
             this.Controls.Add(this.btnCreateMeeting);
             this.Controls.Add(this.btnCreateRoom);
             this.Controls.Add(this.flpRooms);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MeetingRoomMain";
             this.Text = "Toplantı Odası";
             this.ResumeLayout(false);
@@ -108,7 +91,6 @@
         private System.Windows.Forms.Button btnCreateRoom;
         private System.Windows.Forms.Button btnCreateMeeting;
         private System.Windows.Forms.Button btnMeetingCancel;
-        private System.Windows.Forms.Button btnDeleteRoom;
     }
 }
 
